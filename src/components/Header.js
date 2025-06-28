4fb08c1bd35b76ea5c1cbe91ed6d6a05d74c5c48
fercesgim <fernandocesgim@gmail.com>
2025-06-28 13:42:04 -0300

    Logo Gilgamesh

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = () => (
   <header className="text-center my-5">
-    <h1 className="display-4">Gilgamesh</h1>
-    <p className="lead text-muted">Seu assistente de documentação de repositórios</p>
+    <img src="/img/Gilgasvgicon.svg" class="w-25 h-auto" alt="Gilgamesh"></img>
+    <p className="lead text-muted">Seu assistente de documentação de repositórios Java</p>
   </header>
 );
 
